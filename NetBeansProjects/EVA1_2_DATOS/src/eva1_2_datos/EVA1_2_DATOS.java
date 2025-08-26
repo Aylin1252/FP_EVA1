cb0f5be0bbd57b929ac79501def623e0638a6c55
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-08-26 17:52:06 -0600

    Add files via upload

---
+++ NetBeansProjects/EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // //-->COMENTARIO DE UNA LINEA
+        //; --> FIN DE INSTRUCCION
+        System.out.println("RUBEN HERNANDEZ");
+        System.out.println(1000);//numero entero
+        System.out.println(3.1416); //numero decimal
+        System.out.println(true); //valores logicos  VERDERO O FALSO
+        System.out.println('A'); //caracter
+        System.out.println("A"); //CADENA DE TEXTO
+    }
+    
+}
---
+++ NetBeansProjects/EVA1_PRACT3_PRINT/src/eva1_pract3_print/EVA1_PRACT3_PRINT.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_pract3_print;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_PRACT3_PRINT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("NOMBRE DEL ESTUDIANTE:");
+        System.out.println("AYLIN CORDERO");
+        System.out.print("NUMERO DE CONTROL:");
+        System.out.println("2555699");
+        System.out.print("EDAD:");
+        System.out.println(17);
+        
+    }
+    
+}
