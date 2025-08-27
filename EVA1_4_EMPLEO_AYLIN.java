7f033798e8be85db38dc63e172f6ec0854cbeb08
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-08-27 17:07:26 -0600

    Add files via upload

---
+++ EVA1_4_EMPLEO_AYLIN.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_empleo_aylin;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_4_EMPLEO_AYLIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("EMPLEO:");
+        System.out.println("INGENIERO EN INFORMATICA");
+        System.out.print("SALARIO:");
+        System.out.println("20,000");
+        System.out.print("HORAS EN SEMANA:");
+        System.out.println("40");
+        System.out.print("REQUIERE INGLES:");
+        System.out.println("si");
+    }
+    
+}
