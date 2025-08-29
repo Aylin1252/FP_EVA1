c0784e3d74cf3162d90ec9fa116db51acc91bda3
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-08-28 18:00:03 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double f,c;
+        c=7;
+        f = c *1.8+32;
+        System.out.println("cuanto es f cuando c=8 *1.8 +32 es");
+        System.out.println(f);
+                
+        
+    }
+    
+}
