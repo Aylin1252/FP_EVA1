bdd0a143260fa8c414cbe25314cab115c8af2e8b
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-09-01 17:36:53 -0600

    Add files via upload

---
+++ EVA1_81_TEMPERATURA/src/eva1_81_temperatura/EVA1_81_TEMPERATURA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_81_temperatura;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_81_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double f,c;
+        f = 50;
+        c = 5*(f-32)/9;
+        System.out.println("Cuando f=18 cuanto es c son:");
+        System.out.println(c);
+        
+        
+    }
+    
+}
---
+++ EVA1_82_TEMPERATURA/src/eva1_82_temperatura/EVA1_82_TEMPERATURA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_82_temperatura;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_82_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double f,c;
+        c=34;
+        f=((9*c)/5)+32;
+        System.out.println("Cuando c=34 en f son:");
+        System.out.println(f);
+    }
+    
+}
