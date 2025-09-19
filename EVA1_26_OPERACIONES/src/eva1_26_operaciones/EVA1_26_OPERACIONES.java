72f8a25a2c7a9822df7bc3a8a0d69668b7300105
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-09-19 11:02:30 -0600

    Add files via upload

---
+++ EVA1_26_OPERACIONES/src/eva1_26_operaciones/EVA1_26_OPERACIONES.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_26_operaciones;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_26_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int val1 =7,val2 =3;
+        int resu;
+        resu = val1/val2;
+        System.out.println("7/3");
+        System.out.println("resu");
+        
+        double v1 =7.0, v2 =3.0;
+        double res;
+        res = v1/v2;
+        System.out.println("7.0/3.0=");
+        System.out.println("res");
+        
+        int num1 =2024, num2 =4;
+        int resi;
+        resi = num1 % num2;
+        System.out.println("Residuo de 2024%4=");
+        
+        System.out.println("resi");
+        if(resi ==0){
+            System.out.println("puede que sea bisiesto!!");
+       
+                    
+        }
+        
+    }
+    
+}
