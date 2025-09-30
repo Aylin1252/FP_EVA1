c11671e7bdcc7c69ffafc8060c19482a4f7576d9
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-09-29 18:39:31 -0600

    Add files via upload

---
+++ Exa1_eva1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg25550699.exa1_eva1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Exa1_eva1 {
+  
+      
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    Scanner captu =new Scanner (System.in);
+            int natacion,ciclismo,carrera;
+             System.out.println("natacion");
+            natacion =captu.nextInt();
+            if (natacion>=120 <=60); 
+            
+    System.out.println("ciclismo");
+    ciclismo =captu.nextInt();
+        System.out.println("clasificacion excelente");
+    
+
+        System.out.println("clasificacion buena");
+           
+System.out.println("clasificacion regular");
+
